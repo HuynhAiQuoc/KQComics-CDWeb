64cefb5234d028393d98e0106025e6a97b8eac6d
HuynhAiQuoc <huynhaiquoc123@gmail.com>
2022-06-07 15:34:53 +0700

    Update: project
    None

---
+++ ChuyenDeWeb/assets/js/close.js
@@ -0,0 +1,10 @@
+const closePopup = document.getElementById("ant-modal-root");
+
+
+function closeQRCode(){
+    closePopup.style.display = "none";
+}
+
+function openQRCode(){
+    closePopup.style.display = "block";
+}
---
+++ ChuyenDeWeb/assets/js/scroll.js
@@ -0,0 +1,9 @@
+function scrollLeft(){
+    var left = document.querySelector(".HvUBk");
+    left.scrollBy(350, 0);
+}
+
+function scrollRight(){
+    var right = document.getElementById(".HvUBk");
+    right.scrollBy(-350, 0);
+}
